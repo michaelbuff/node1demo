2f8a814c0a13acd100be528276196eabcdd2ab40
michaelbuff <michael@buffington.dev>
2022-10-28 15:22:40 -0600

    Adding MERN 1 assignment

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 const http = require("http");
 
-http.createServer(function(request, response) {    
-   response.writeHead(200, {"Content-Type": "text/html"});
-   response.write("<h1>Hello, Node.js!</h1>");    
-   response.write("<h2>Michael's First Project</h2>");
-   response.write('<h2>click <a href="localhost:3006">here</a> to visit my other site</h2>');
-   response.write("<p>why doesn't this linking work great?</p>")
-   response.end();
-}).listen(3000);
 
 http.createServer(function(request, response) {    
     response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("<h1>Welcome to my other website!</h1>");    
-    response.write('<h2>click <a href="localhost:3000">here</a> to visit my other site</h2>');
-    response.write("<p>why doesn't this linking work great?</p>")
+    response.write("<h1>Hello  NodeJS</h1>");    
+    response.write("<h3>Michael Buffington's (m49v138) MERN1 Assignment</h3>");
+    response.write("<p>brought to you on <i>port 3006</i></p>")
     response.end();
  }).listen(3006);
